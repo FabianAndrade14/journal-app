cb995228b7f38416ac550f50f91b80a776c4a3f0
FabianAndrade14 <fabianandresm10@gmail.com>
2025-01-30 17:17:27 -0500

    feat(main): Added the material UI configuration

---
+++ src/theme/AppTheme.jsx
@@ -0,0 +1,15 @@
+import { CssBaseline, ThemeProvider } from "@mui/material"
+import { purpleTheme } from "./purpleTheme"
+
+
+export const AppTheme = ({ children }) => {
+  return (
+    <ThemeProvider theme={ purpleTheme }>
+
+        <CssBaseline />
+        { children }
+    </ThemeProvider>
+  )
+}
+
+
