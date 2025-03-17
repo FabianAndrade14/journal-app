38fba6b5c849cf9ccd5d2cbcf01ca6d8c1a9cd9c
FabianAndrade14 <fabianandresm10@gmail.com>
2025-03-17 16:53:24 -0500

    fix(main): Showing the notes in the sidebar componente

---
+++ src/journal/components/SideBarItem.jsx
@@ -0,0 +1,25 @@
+import { TurnedInNot } from '@mui/icons-material'
+import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
+import React from 'react'
+import { useMemo } from 'react'
+
+export const SideBarItem = ({ title = '', body, id }) => {
+
+    const newTitle = useMemo(() => {
+        return title.length > 17 ? title.substring(0, 17) + '...' : title;
+    }, [title])
+
+    return (
+        <ListItem disablePadding>
+            <ListItemButton>
+                <ListItemIcon>
+                    <TurnedInNot></TurnedInNot>
+                </ListItemIcon>
+                <Grid container>
+                    <ListItemText primary={newTitle}></ListItemText>
+                    <ListItemText secondary={body}></ListItemText>
+                </Grid>
+            </ListItemButton>
+        </ListItem>
+    )
+}
