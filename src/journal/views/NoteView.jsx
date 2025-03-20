74ed62ea69fcc23d690ecef1505028b8d1e7f863
FabianAndrade14 <fabianandresm10@gmail.com>
2025-03-19 19:22:12 -0500

    fix(main): Added sweetalert to the project after the note updating thing

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -3,15 +3,17 @@ import { Button, Grid, TextField, Typography } from "@mui/material"
 import { ImageGallery } from "../components"
 import { useDispatch, useSelector } from "react-redux"
 import { useForm } from '../../hooks'
-import { useMemo } from "react"
+import { useEffect, useMemo } from "react"
 import { setActiveNote } from "../../store/journal/journalSlice"
 import { startSaveNote } from "../../store/journal/thunks"
+import Swal from "sweetalert2"
+import 'sweetalert2/dist/sweetalert2.css'
 
 export const NoteView = () => {
 
     const dispatch = useDispatch();
 
-    const { active:note } = useSelector( state => state.journal );
+    const { active:note, messageSaved, isSaving } = useSelector( state => state.journal );
 
     const {body, title, date, onInputChange, formState} = useForm(note);
 
@@ -25,6 +27,12 @@ export const NoteView = () => {
     useEffect( () => {
         dispatch( setActiveNote(formState) );
     }, [formState])
+
+    useEffect( () => {
+        if ( messageSaved.length > 0 ) {
+            Swal.fire('Nota actualizada', messageSaved, 'success');
+        }
+    }, [messageSaved])
 
     const onSaveNote = () => {
         dispatch( startSaveNote() );
@@ -43,6 +51,7 @@ export const NoteView = () => {
             </Grid>
             <Grid item>
                 <Button 
+                disabled={ isSaving }
                 onClick={ onSaveNote }
                 color="primary" 
                 sx={{ padding: 2 }}
--- src/store/journal/journalSlice.js
+++ src/store/journal/journalSlice.js
@@ -25,16 +25,28 @@ export const journalSlice = createSlice({
         },
         setActiveNote: (state, action) => {
             state.active = action.payload;
+            state.messageSaved = '';
         },
         setNotes: (state, action) => {
             state.notes = action.payload;
         },
         setSaving: ( state ) => {
             state.isSaving = true;
+            state.messageSaved = '';
         },
         updateNotes: (state, action) => {
             state.isSaving = false;
-            state.notes = state.notes.map()
+            state.notes = state.notes.map( note => {
+
+                if( note.id === action.payload.id ) {
+                    return action.payload;
+                }
+
+                return note;
+
+            });
+
+            state.messageSaved = `${ action.payload.title }, actualizada correctamente`;
         },
         deleteNodeById: (state, action) => {
 
